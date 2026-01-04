4a6213386b8c01b7f2869e405acc6d07b21178ab
Mattu0408 <hardik.pandya9393@gmail.com>
2026-01-04 12:28:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state2"
+    bucket = "project-terraform-mymattu1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
